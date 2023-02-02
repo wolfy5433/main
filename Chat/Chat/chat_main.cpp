118cd4853f73e82f0b0a70e7a93f450a1aca37ac
Wolfy(Nikita) <volkovnikitosik2003@gmail.com>
2023-02-03 01:11:34 +0300

    Добавлено групповое сообщение,выход из аккаунта при желании

--- Chat/Chat/chat_main.cpp
+++ Chat/Chat/chat_main.cpp
@@ -9,16 +9,16 @@ int main()
 	string userName, login;
 	int password;
 	int i = 0;
-	int open;
+	char open;
 	cout << "Выберите,что вы хотите сделать" << endl;
 	cout << "1.Создать аккаунт" << endl << "2.Войти в аккаунт" << endl << "3.Отобразить всех пользователей" << endl << "4.Выйти из чата" << endl;
 	cin >> open;
 	clearCin();
-	while (open != 4)
+	while (open != '4')
 	{
 		switch (open)
 		{
-		case 1:
+		case '1':
 		{
 			cout << "Введите имя: ";
 			getline(cin, userName);
@@ -33,7 +33,7 @@ int main()
 			++size;
 			break;
 		}
-		case 2:
+		case '2':
 		{
 			if (size == 1)
 			{
@@ -52,51 +52,87 @@ int main()
 				b = data[i].Check(login, password);
 				if (b == true)
 				{
+					string name = data[i].GetUserName();
 					char choiceInsideCase2;
-					cout << "Выберите действие:" << endl << "1.Написать сообщение" << endl << "2.Показать сообщение" << endl;
+					cout << "Выберите действие:" << endl << "1.Написать сообщение" << endl << "2.Показать сообщение" << endl << "3.Выйти из аккаунта" << endl;
 					cin >> choiceInsideCase2;
 					clearCin();
-					switch (choiceInsideCase2)
+					while (choiceInsideCase2 != '3')
 					{
-					case '1':
-					{
-						cout << "Имена пользователей:" << endl;
-						for (int j = 0; j < i; ++j)
+						switch (choiceInsideCase2)
 						{
-							cout << '-' << data[j].GetUserName() << endl;
-						}
-						for (int j = i + 1; j < size - 1; ++j)
+						case '1':
 						{
-							cout << '-' << data[j].GetUserName() << endl;
-						}
-						cout << endl;
-						cout << "Введите имя пользователя кому хотите написать сообщение: ";
-						getline(cin, userName);
-						for (int k = 0; k < size - 1; ++k)
-						{
-							if (userName == data[k].GetUserName())
+							char choice2;
+							cout << "Кому вы хотите написать сообщение:" << endl << "1.Определенному пользователю" << endl << "2.Групповое сообщение" << endl;
+							cin >> choice2;
+							clearCin();
+							switch (choice2)
+							{
+							case '1':
+							{
+								cout << "Имена пользователей:" << endl;
+								for (int j = 0; j < i; ++j)
+								{
+									cout << '-' << data[j].GetUserName() << endl;
+								}
+								for (int j = i + 1; j < size - 1; ++j)
+								{
+									cout << '-' << data[j].GetUserName() << endl;
+								}
+								cout << endl;
+								cout << "Введите имя пользователя кому хотите написать сообщение: ";
+								getline(cin, userName);
+								for (int k = 0; k < size - 1; ++k)
+								{
+									if (userName == data[k].GetUserName())
+									{
+										string message;
+										cout << "Введите сообщение: ";
+										getline(cin, message);
+										message = "У вас сообщение от пользователя " + data[i].GetUserName() + ": " + '\"' + message + '\"';
+										data[k].SetMessage(message);
+									}
+								}
+								break;
+							}
+							case '2':
 							{
 								string message;
+								string groupmessage;
 								cout << "Введите сообщение: ";
 								getline(cin, message);
-								message = "У вас сообщение от пользователя " + data[i].GetUserName() + ": " + '\"' + message + '\"';
-								data[k].SetMessage(message);
+								for (int k = 0; k < size - 1; ++k)
+								{
+									groupmessage = ' ';
+									if (name != data[k].GetUserName())
+									{
+										groupmessage = "У вас сообщение от пользователя " + data[i].GetUserName() + ": " + '\"' + message + '\"';
+										data[k].SetMessage(groupmessage);
+									};
+								}
+								break;
 							}
+							}
+							break;
 						}
-						break;
-					}
-					case '2':
-					{
-						cout << data[i].GetMessage() << '\n' << endl;
-						system("pause>0");
-						break;
-					}
+						case '2':
+						{
 
-					default:
-					{
-						cout << "Выход" << endl;
-					}
-					}
+							cout << data[i].GetMessage() << '\n' << endl;
+							/*system("pause>0");*/
+							break;
+						}
+						case '3':
+						{
+							cout << "Выход из аккаунта произведен" << endl;
+							break;
+						}
+					    }
+						cout << "Выберите действие:" << endl << "1.Написать сообщение" << endl << "2.Показать сообщение" << endl << "3.Выйти из аккаунта" << endl;
+					cin >> choiceInsideCase2;
+					clearCin();
+					};
 					break;
 				};
 			};
@@ -106,7 +142,7 @@ int main()
 			};
 			break;
 		}
-		case 3:
+		case '3':
 		{
 			if (size == 1)
 			{
@@ -119,7 +155,7 @@ int main()
 				cout << "Аккаунт номер " << i + 1 << ':' << '\n' << data[i] << endl;
 			}
 		}
-		case 4:
+		case '4':
 		{
 			break;
 		};
